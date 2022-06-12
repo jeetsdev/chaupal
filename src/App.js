2b7762f2190bc5922c071f1e7355665ba2b31a8e
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-06-12 21:59:56 +0530

    fix : data featching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
 import { MyToast, NewPostModal } from "./components";
+import { getAllPost } from "./features/Post/PostSlice";
 import { NavigationRoutes } from "./routes/NavigationRoutes";
 
 function App() {
+	// Getting all post here
+    const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getAllPost());
+	}, [dispatch]);
+
 	return (
 		<div id="main-app">
 			<MyToast />
