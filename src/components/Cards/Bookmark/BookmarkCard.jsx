0b659e06e99cfdf1ed616c8db3b65e259fd03b02
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-07-17 09:09:49 +0530

    fix : bookmarked post

--- src/components/Cards/Bookmark/BookmarkCard.jsx
+++ src/components/Cards/Bookmark/BookmarkCard.jsx
@@ -2,6 +2,7 @@ import { BsFillBookmarkDashFill, BsThreeDotsVertical } from "react-icons/bs"
 import { useDispatch, useSelector } from "react-redux"
 import { useState } from "react"
 import { removeFromBookmark } from "../../../features/Post/PostSlice"
+import { useNavigate } from "react-router-dom"
 
 export const BookmarkCard = ({ post }) => {
 
@@ -9,16 +10,21 @@ export const BookmarkCard = ({ post }) => {
     const { allUsers } = useSelector(state => state.user);
     const [postMenu, setPostMenu] = useState(false);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const currentPostUser = allUsers.find(user => user.username === post.username);
 
-
-    const bookmarkHandler = () => {
+    const bookmarkHandler = (event) => {
+        event.stopPropagation();
         dispatch(removeFromBookmark({ postID: post._id, authToken }));
         setPostMenu(prev => !prev);
     }
 
+    const postClickHandler = (postId) => {
+        navigate(`/post/${postId}`)
+    }
+
     return (
-        <main className="flex g-secondary p-4 mx-8 bg-white my-2 rounded z-10">
+        <main className="flex post-card p-4 mx-8 bg-white my-2 rounded z-10 hover:cursor-pointer" onClick={() => postClickHandler(post._id)}>
             <section>
                 <img src={currentPostUser?.avatar} alt="" className="w-10 h-10 mr-4 rounded-full object-cover border-2 p-px" />
             </section>
@@ -32,11 +38,14 @@ export const BookmarkCard = ({ post }) => {
                 </p>
                 <div className="flex justify-between text-xl mt-6 relative">
                     {/* Menu button here */}
-                    <BsThreeDotsVertical className="w-5 h-5 ml-auto rounded-full text-xl p-px  hover:cursor-pointer hover:text-blue-500 transition-colors" onClick={() => setPostMenu(prev => !prev)} />
+                    <BsThreeDotsVertical className="w-5 h-5 ml-auto rounded-full text-xl p-px  hover:cursor-pointer hover:text-blue-500 transition-colors" onClick={(event) => {
+                        event.stopPropagation();
+                        setPostMenu(prev => !prev);
+                    }} />
 
                     {/* Post menu here */}
                     <div className={"post-menu absolute z-0 flex justify-center items-center gap-1 bg--200 -top-1 right-6 rounded bg- " + (postMenu ? "show-menu" : "hide-menu")}>
-                        <BsFillBookmarkDashFill className="text-3xl rounded btn-icon hover:cursor-pointer  transition-colors p-1" onClick={bookmarkHandler} />
+                        <BsFillBookmarkDashFill className="text-3xl rounded btn-icon hover:cursor-pointer  transition-colors p-1 bg-primary" onClick={bookmarkHandler} />
                     </div>
                 </div>
             </section>
