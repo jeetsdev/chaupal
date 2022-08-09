b3123671f003dc161383396b7e4b1e4f6671e391
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-08-09 17:35:55 +0530

    removed unused icon

--- src/components/Cards/Comment/CommentCard.jsx
+++ src/components/Cards/Comment/CommentCard.jsx
@@ -52,7 +52,6 @@ export const CommentCard = ({ comment, postID }) => {
             </section>
             {comment?.username === username && <section className='ml-auto flex items-end'>
                 <MdDelete className='text-xl mx-1 text-zinc-700 hover:cursor-pointer btn-icon' onClick={deleteCommentHandler} />
-                <MdModeEditOutline className='text-xl mx-1 text-zinc-700 hover:cursor-pointer btn-icon' />
             </section>}
         </main>
     )
