a38e3fe9f7ad61c8847d9c87a4b71858f2282385
Ranjeet Singh <ranjeet1.singh@cars24.com>
2025-03-09 00:29:11 +0530

    ref: updated profile url

--- src/utils/avatarDB.js
+++ src/utils/avatarDB.js
@@ -1,18 +1,18 @@
 export const avatarDB = {
-	chaupalAvatar:
-		"https://res.cloudinary.com/dz7duml2h/image/upload/v1654810082/2137px-Coffee_cup_icon.svg_ujzhhm.png",
-	dedsecAvatar:
-		"https://res.cloudinary.com/dz7duml2h/image/upload/v1654809081/51mBV7e37rL._AC_SY450__bznsbp.jpg",
-	jeetsdevAvatar:
-		"https://res.cloudinary.com/dz7duml2h/image/upload/v1654812234/Screenshot_from_2022-06-05_14-24-12_uymhkw.png",
-	adiiAvatar:
-		"https://res.cloudinary.com/dz7duml2h/image/upload/v1655812872/anime-girl-brunette-face-wallpaper-thumb_jwbbce.jpg",
-	harviAvatar:
-		"https://res.cloudinary.com/dz7duml2h/image/upload/v1655812947/3b990dd21439d920db38a32e46e1ad92--batman-dc-comics-anime-comics_iz7hvv.jpg",
-	ninjaAvatar:
-		"https://res.cloudinary.com/dz7duml2h/image/upload/v1655813029/d5c611e6da7b3ce6aeded61fe013ad19_unp2ns.jpg",
-	capAvatar:
-		"https://res.cloudinary.com/dz7duml2h/image/upload/v1655813162/download_kvwwzp.jpg",
-	tonyAvatar:
-		"https://res.cloudinary.com/dz7duml2h/image/upload/v1655813192/images_xhru8q.jpg",
+  chaupalAvatar:
+    "https://res.cloudinary.com/drkcsvbdj/image/upload/v1728022225/samples/animals/cat.jpg",
+  dedsecAvatar:
+    "https://res.cloudinary.com/drkcsvbdj/image/upload/v1728022233/samples/look-up.jpg",
+  jeetsdevAvatar:
+    "https://res.cloudinary.com/drkcsvbdj/image/upload/v1728022233/samples/man-portrait.jpg",
+  adiiAvatar:
+    "https://res.cloudinary.com/drkcsvbdj/image/upload/v1728022234/samples/woman-on-a-football-field.jpg",
+  harviAvatar:
+    "https://res.cloudinary.com/drkcsvbdj/image/upload/v1728022235/cld-sample.jpg",
+  ninjaAvatar:
+    "https://res.cloudinary.com/drkcsvbdj/image/upload/v1728022234/samples/dessert-on-a-plate.jpg",
+  capAvatar:
+    "https://res.cloudinary.com/drkcsvbdj/image/upload/v1728022233/samples/outdoor-woman.jpg",
+  tonyAvatar:
+    "https://res.cloudinary.com/drkcsvbdj/image/upload/v1728022233/samples/man-on-a-street.jpg",
 };
